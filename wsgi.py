5a81902a79d13181d4ad39f36c1aba189f8a0a8d
Fernando Roman Garcia <fer.roman92@gmail.com>
2017-02-28 00:50:24 +0100

    Try to fix environment

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 import os
-from src import application
+import sys
 
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+sys.path.append(os.environ['OPENSHIFT_REPO_DIR'] + "/src")
+
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
@@ -12,10 +14,12 @@ except IOError:
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
+from src import application
 
 #
 # Below for testing only
 #
+
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 8051, application)

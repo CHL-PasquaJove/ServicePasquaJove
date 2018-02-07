5f44477204f1117710d90b37bfc749abcb516c18
Fernando Roman Garcia <fer.roman92@gmail.com>
2018-02-07 22:24:39 +0100

    Remove CORS from app file

--- src/app.py
+++ src/app.py
@@ -2,23 +2,6 @@
 import falcon
 import pascua
 
-
-def cors_middleware(request, response, params):
-    response.set_header(
-        'Access-Control-Allow-Origin',
-        '*'
-    )
-    response.set_header(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Api-Token'
-    )
-    # This could be overridden in the resource level
-    response.set_header(
-        'Access-Control-Expose-Headers',
-        'Location'
-    )
-
-
 # falcon.API instances are callable WSGI apps
-app = falcon.API(before=[cors_middleware])
+app = falcon.API()
 pascua.init(app)
